a2266ad457c3875527a84698dbcbb7505b8382ff
Julie Freund-Poulsen <juliefreund-poulsen@lenovo-pc.kea.local>
2018-11-15 11:53:36 +0100

    bygget videre på js og css

--- script.js
+++ script.js
@@ -45,10 +45,35 @@ function playingGame() {
 //FIGURELEMENTER
 function screenClick() {
     console.log("screenClick");
+    if (this.classList.contains("nude")) {
+            console.log("du har klikket på nude");
+
+//            life--;
+//            console.log("life");
+//            document.querySelector("#heart" + (life + 1)).classList.add("hide");
+//            //gør at et hjerte fjernes af gangen
+//            console.log(this);
+
+
+        } else if (this.classList.contains("normal")) {
+            console.log("du har klikket på normal");
+//            points++;
+//            console.log("points");
+//            document.querySelector("#points").innerHTML = points;
+//            console.log(this);
+
+        }
+        this.classList.add("dissappear");
+        this.addEventListener("animationend", newPic);
+        gameStatus();
 }
 
 function newPic() {
     console.log("newPic");
+        this.className = "";
+        //^betyder at klasserne forsvinder
+        console.log("random");
+        this.classList.add("billeder" + Math.floor((Math.random() * 8) + 1));
 }
 
 //MUSIK OG LYD START
@@ -82,6 +107,12 @@ function musicOn() {
 //GAMESTATUS START
 function gameStatus() {
     console.log("gameStatus");
+console.log(life);
+    if (life == 0) {
+        document.querySelector("#gameover").classList.remove("hide");
+    } else if (points == 7) {
+        document.querySelector("#levelcomplete").classList.remove("hide");
+    }
 }
 
 function gameOver() {
