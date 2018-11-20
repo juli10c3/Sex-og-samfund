ffc88b806a0d5c6e53b41e3086794a57945f7cb5
Julie Freund-Poulsen <juliefreund-poulsen@ceres-pc.kea.local>
2018-11-20 12:26:22 +0100

    mangler kun timer + musik

--- script.js
+++ script.js
@@ -19,7 +19,8 @@ function showStart() {
     document.querySelector("#start").classList.remove("hide");
     document.querySelector("#play").classList.add("pulse");
     document.querySelector("#play").addEventListener("click", hideStart);
-
+    document.querySelector("#sfx").addEventListener("click", toggleSounds);
+    document.querySelector("#music").addEventListener("click", toggleMusic);
 }
 
 
@@ -31,9 +32,7 @@ function hideStart() {
     document.querySelector("#start").classList.add("fade_out");
     document.querySelector("#start").classList.add("hide");
     document.querySelector("#start").addEventListener("animationend", startGame);
-    document.querySelector("#sfx").addEventListener("click", toggleSounds);
-    document.querySelector("#music").addEventListener("click", toggleMusic);
-    document.querySelector("#phone_btn").classList.remove("hide");
+
 
 
 }
@@ -50,9 +49,10 @@ function startGame() {
     //    document.querySelector("#phone_btn").addEventListener("click", screenClick);
     //    document.querySelector("#phone_btn").classList.add("pulse");
     document.querySelector("#picture").addEventListener("click", screenClick);
-//    document.querySelector("#picture2").addEventListener("click", screenClick);
-//    document.querySelector("#picture3").addEventListener("click", screenClick);
-//    document.querySelector("#picture4").addEventListener("click", screenClick);
+
+    //    document.querySelector("#picture2").addEventListener("click", screenClick);
+    //    document.querySelector("#picture3").addEventListener("click", screenClick);
+    //    document.querySelector("#picture4").addEventListener("click", screenClick);
 
 
 }
@@ -64,49 +64,142 @@ function startGame() {
 //FIGURELEMENTER
 function screenClick() {
     console.log("screenClick");
-    if (this.classList.contains("nude")) {
+    if (this.classList.contains("type1")) {
         console.log("du har klikket på nude");
+        document.querySelector("#sfx1").play();
+        document.querySelector("#sfx1").currentTime = 0;
+        console.log("life");
 
         life--;
-        console.log("life");
-//        document.querySelector("#energy" + (life + 1)).classList.add("hide");
-        //gør at et hjerte fjernes af gangen
-        console.log(this);
-                this.classList.add("hide");
-
-
-
-    } else if (this.classList.contains("normal")) {
-        console.log("du har klikket på normal");
-        points++;
-        console.log("points");
-        document.querySelector("#points").innerHTML = points;
+        //        console.log("life");
+        //        document.querySelector("#energy" + (life + 1));
+        //gør at et hjerte fjernes af gangenf
         console.log(this);
         this.classList.add("hide");
 
+        //        nulstil();
+    } else if (this.classList.contains("type2")) {
+        console.log("du har klikket på normal");
+        document.querySelector("#sfx1").play();
+        document.querySelector("#sfx1").currentTime = 0;
+
+        console.log("points");
+        points++;
+        //        console.log("points");
+        //        document.querySelector("#points").innerHTML = points;
+        console.log(this);
+        this.classList.add("hide");
+        //        nulstil();
+
+    } else if (this.classList.contains("type3")) {
+        console.log("du har klikket på normal");
+        document.querySelector("#sfx1").play();
+        document.querySelector("#sfx1").currentTime = 0;
+        console.log("points");
+
+        points++;
+        //        console.log("points");
+        //        document.querySelector("#points").innerHTML = points;
+        console.log(this);
+        this.classList.add("hide");
+        //        nulstil();
+
+    } else if (this.classList.contains("type4")) {
+        console.log("du har klikket på normal");
+        document.querySelector("#sfx1").play();
+        document.querySelector("#sfx1").currentTime = 0;
+        console.log("points");
+        points++;
+        //        document.querySelector("#points").innerHTML = points;
+        console.log(this);
+        this.classList.add("hide");
+        //        nulstil();
+
+    } else if (this.classList.contains("type5")) {
+        console.log("du har klikket på normal");
+        document.querySelector("#sfx1").play();
+        document.querySelector("#sfx1").currentTime = 0;
+        console.log("points");
+
+        points++;
+
+        //        document.querySelector("#points").innerHTML = points;
+        console.log(this);
+        this.classList.add("hide");
+        //        nulstil();
+
+    } else if (this.classList.contains("type6")) {
+        console.log("du har klikket på normal");
+        document.querySelector("#sfx1").play();
+        document.querySelector("#sfx1").currentTime = 0;
+        console.log("points");
+        points++;
+
+        //        document.querySelector("#points").innerHTML = points;
+        console.log(this);
+        this.classList.add("hide");
+        //        nulstil();
+
+    } else if (this.classList.contains("type7")) {
+        console.log("du har klikket på normal");
+        document.querySelector("#sfx1").play();
+        document.querySelector("#sfx1").currentTime = 0;
+        console.log("points");
+        points++;
+
+        //        document.querySelector("#points").innerHTML = points;
+        console.log(this);
+        this.classList.add("hide");
+        //        nulstil();
+
+    } else if (this.classList.contains("type8")) {
+        console.log("du har klikket på nude");
+        document.querySelector("#sfx1").play();
+        document.querySelector("#sfx1").currentTime = 0;
+        console.log("life");
+        life--;
+
+        //        document.querySelector("#energy" + (life + 1));
+        //gør at et hjerte fjernes af gangenf
+        console.log(this);
+        this.classList.add("hide");
+
+        //        nulstil();
 
     }
     this.classList.add("disappear");
     this.addEventListener("animationend", newPic);
-//    gameStatus();
+    gameStatus();
 }
+
+
+//function newNude() {
+//
+//}
+
+//function nulstil() {
+//    console.log("nulstil");
+//    document.querySelector("#picture").removeAttribute("class");
+//    newPic();
+//    setTimeout(3000);
+//
+//}
 
 function newPic() {
     console.log("newPic");
-      this.className = "";
-        //^betyder at klasserne forsvinder
-        console.log("random");
-        this.classList.add("normal" + Math.floor((Math.random() * 3) + 1));
+    this.className = "";
+    //^betyder at klasserne forsvinder
+    console.log("random");
+    this.classList.add("type" + Math.floor((Math.random() * 8) + 1)); //    document.querySelector("#picture" + myRandom()).classList.add("type");
+    //}
+    //
+    //function myRandom() {
+    //    console.log("my random")
+    //    let tilfaeldigttal = Math.floor(Math.random() * 4) + 1;
+    //    return tilfaeldigttal;
 }
-//    document.querySelector("#picture" + myRandom()).classList.add("normal");
-//}
-//
-//function myRandom() {
-//    console.log("my random")
-//    let tilfaeldigttal = Math.floor(Math.random() * 4) + 1;
-//    return tilfaeldigttal;
-//}
 //MUSIK OG LYD START
+
 
 function toggleSounds() {
     console.log("toggleSounds");
@@ -126,14 +219,14 @@ function toggleSounds() {
 function soundsOff() {
     console.log("soundsOff");
     document.querySelector("#sfx").classList = "sfx_off";
-    //    document.querySelector("#sfx1").muted = true;
+    document.querySelector("#sfx1").muted = true;
     //    document.querySelector("#sfx2").muted = true;
 }
 
 function soundsOn() {
     console.log("soundsOn");
     document.querySelector("#sfx").classList = "sfx_on";
-    //    document.querySelector("#sfx1").muted = false;
+    document.querySelector("#sfx1").muted = false;
     //    document.querySelector("#sfx2").muted = false;
 
 }
